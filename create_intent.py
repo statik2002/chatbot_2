3080781fa55baaa61037d784a70552c521785692
statik2002 <statik2002@mail.ru>
2023-01-23 16:07:54 +0800

    add DialogFlow learning script

---
+++ create_intent.py
@@ -0,0 +1,45 @@
+import json
+import os
+
+from dotenv import load_dotenv
+
+
+def create_intent(project_id, display_name, training_phrases_parts, message_texts):
+    """Create an intent of the given intent type."""
+    from google.cloud import dialogflow
+
+    intents_client = dialogflow.IntentsClient()
+
+    parent = dialogflow.AgentsClient.agent_path(project_id)
+    training_phrases = []
+    for training_phrases_part in training_phrases_parts:
+        part = dialogflow.Intent.TrainingPhrase.Part(text=training_phrases_part)
+        # Here we create a new training phrase for each provided part.
+        training_phrase = dialogflow.Intent.TrainingPhrase(parts=[part])
+        training_phrases.append(training_phrase)
+
+    text = dialogflow.Intent.Message.Text(text=message_texts)
+    message = dialogflow.Intent.Message(text=text)
+
+    intent = dialogflow.Intent(
+        display_name=display_name, training_phrases=training_phrases, messages=[message]
+    )
+
+    response = intents_client.create_intent(
+        request={"parent": parent, "intent": intent}
+    )
+
+    print("Intent created: {}".format(response))
+
+
+load_dotenv()
+project_id = os.environ['GOOGLE_CLOUD_PROJECT_ID']
+with open('questions.json', 'r') as intents_file:
+        intents = json.load(intents_file)
+
+for intent, phrases in intents.items():
+
+        try:
+            create_intent(project_id, intent, phrases['questions'], phrases['answer'])
+        except :
+            print('Error')
